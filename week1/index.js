a554ed46b92c503e89dd3f8a481551b60c0b52a4
kedugaw <kedugaw@gmail.com>
2025-03-22 21:04:08 +0800

    update 題目二：變數命名練習

--- week1/index.js
+++ week1/index.js
@@ -19,10 +19,10 @@ console.log(alexAge, alexMembershipID, isRunningOnTreadmill);
 // 情境：Alex 這個月的運動預算有 3000 元
 // 請修改以下中文變數名稱，讓他符合變數語意
 
-const 變數一 = 300;
-const 變數二 = 500;
-const 變數三 = 1500;
-const 變數四 = 3000;
+const yogaGroupPrice = 300;
+const weightTrainingGroupPrice = 500;
+const weightTraining1to1Price = 1500;
+const monthSportsBudget = 3000;
 
 // ### 題目三：變數計算
 // 呈上題，Alex 想要規劃好自己的運動課程，需符合以下三個條件，請將花費總數計算在 AlexBudget 上，一起幫幫他吧！
